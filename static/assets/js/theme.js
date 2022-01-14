c60184720c709f0510b66cd019bdc480c6e5746a
Michael Kuc <michaelkuc6@gmail.com>
2022-01-14 13:15:04 +0000

    feat(theme): avoid flash-of light theme
    Add code to preload the style background to prevent a flash for dark themes while waiting for the theme stylesheet to download.

--- static/assets/js/theme.js
+++ static/assets/js/theme.js
@@ -7,52 +7,67 @@
  */
 
 function updateTheme(theme) {
-    Cookies.set('theme', theme.id, { expires: 365, path: '/', sameSite: 'strict' });
+	Cookies.set('theme', theme.id, { expires: 365, path: '/', sameSite: 'strict' });
 
-    let stylesheet = document.getElementById('theme-stylesheet');
+	let stylesheet = document.getElementById('theme-stylesheet');
+	let tempStylesheet = null;
 
-    if (stylesheet === null) {
-        const head = document.getElementsByTagName('head')[0];
-        stylesheet = document.createElement('link');
-        stylesheet.id = 'theme-stylesheet';
-        stylesheet.rel = 'stylesheet';
-        stylesheet.type = 'text/css';
-        head.appendChild(stylesheet);
-    }
+	if (stylesheet === null) {
+		const head = document.getElementsByTagName('head')[0];
 
-    const current = document.getElementById('theme-current');
-    if (theme.name !== null && current !== null) {
-        current.innerHTML = theme.name;
-    }
+		stylesheet = document.createElement('link');
+		stylesheet.id = 'theme-stylesheet';
+		stylesheet.rel = 'stylesheet';
+		stylesheet.type = 'text/css';
+		head.appendChild(stylesheet);
 
-    stylesheet.href = `/assets/themes/${theme.id}.css`;
+		if (theme.id in themeColours) {
+			tempStylesheet = document.createElement('style');
+			tempStylesheet.innerHTML = `body { --theme-base-background: ${themeColours[theme.id].backgroundColour}; }`;
+			head.appendChild(tempStylesheet);
+		}
+	}
+
+	const current = document.getElementById('theme-current');
+	if (theme.name !== null && current !== null) {
+		current.innerHTML = theme.name;
+	}
+
+	stylesheet.href = `/assets/themes/${theme.id}.css`;
+	stylesheet.onload = function() {
+		if (tempStylesheet !== null) {
+			tempStylesheet.remove();
+			tempStylesheet = null;
+		}
+	}
 }
 
-const theme = Cookies.get('theme') || 'plain';
+const defaultDarkTheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+const theme = Cookies.get('theme') || (defaultDarkTheme ? 'nocturnal' : 'plain');
 updateTheme({
-    id: theme,
+	id: theme,
 });
 
 window.addEventListener('load', function() {
-    const dropdownContainer = document.getElementById('theme-selector');
-    dropdownContainer.style.visibility = 'visible';
+	const dropdownContainer = document.getElementById('theme-selector');
+	dropdownContainer.style.visibility = 'visible';
 
-    const dropdownOptionElem = document.getElementById('theme-dropdown-options');
-    let dropdownOptions = {};
+	const dropdownOptionElem = document.getElementById('theme-dropdown-options');
+	let dropdownOptions = {};
 
-    for (const child of dropdownOptionElem.children) {
-        dropdownOptions[child.dataset.value] = child;
-    }
+	for (const child of dropdownOptionElem.children) {
+		dropdownOptions[child.dataset.value] = child;
+	}
 
-    const current = document.getElementById('theme-current');
-    current.innerText = dropdownOptions[theme].innerHTML;
+	const current = document.getElementById('theme-current');
+	current.innerText = dropdownOptions[theme].innerHTML;
 
-    for (const [optionTheme, dropdownOption] of Object.entries(dropdownOptions)) {
-        dropdownOption.addEventListener('click', function() {
-            updateTheme({
-                id: optionTheme,
-                name: dropdownOption.innerText
-            });
-        });
-    }
+	for (const [optionTheme, dropdownOption] of Object.entries(dropdownOptions)) {
+		dropdownOption.addEventListener('click', function() {
+			updateTheme({
+				id: optionTheme,
+				name: dropdownOption.innerText
+			});
+		});
+	}
 });
